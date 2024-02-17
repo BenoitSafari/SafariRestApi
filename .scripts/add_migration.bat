3d6d47c37ec718c1d55c8bee00183dfffc586ac8
benoitSafari <gregory.michalak.192@gmail.com>
2024-02-17 17:55:00 +0100

    Removed unnecessary scripts and docker

--- .scripts/add_migration.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-IF NOT "%~2"=="" (
-    cd SafariRest.Database
-    dotnet ef migrations add %1 --output-dir Migrations -- %2
-    cd ../..
-) ELSE (
-    echo Usage: %0 ^<MigrationName^> ^<ConnectionString^>
-)
--- .scripts/add_migration.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin
-
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <MigrationName> <ConnectionString>"
-    exit 1
-fi
-cd SafariRest.Database
-dotnet ef migrations add $1 --output-dir Migrations -- $2
-cd ../..
