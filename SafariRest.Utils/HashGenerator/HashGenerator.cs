2d1f4ce0b16c2ccba7213bd4c8f5805455752dce
benoitSafari <gregory.michalak.192@gmail.com>
2024-02-25 22:43:37 +0100

    AuthenticationService

---
+++ SafariRest.Utils/HashGenerator/HashGenerator.cs
@@ -0,0 +1,12 @@
+﻿namespace SafariRest.Utils;
+
+public static class HashGenerator
+{
+    private static readonly int DefaultSalt = 10;
+
+    public static string HashString(string str, int? salt) =>
+        BCrypt.Net.BCrypt.HashPassword(str, salt ?? DefaultSalt);
+
+    public static bool CompareString(string str, string hash) =>
+        BCrypt.Net.BCrypt.Verify(str, hash);
+}
